de019f6cc94d81d6c48ce45261b9c329c49ea349
Aritse-stack <valter.mygame@gmail.com>
2026-03-27 14:10:19 -0300

    fix: change from 'id' to 'data' selectors

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -26,7 +26,8 @@
 
 Cypress.Commands.add('login', (username: string, password: string) => {
   cy.visit('https://www.saucedemo.com')
-  cy.get('#user-name').type(username)
-  cy.get('#password').type(password)
-  cy.get('#login-button').click()
+
+  cy.get('[data-test="username"]').type(username)
+  cy.get('[data-test="password"]').type(password)
+  cy.get('[data-test="login-button"]').click()
 })
