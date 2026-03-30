7c9f4698253dc5877bdaaa3f88d5fbfb4cde235b
Aritse-stack <valter.mygame@gmail.com>
2026-03-30 12:42:32 -0300

    feat: add test in different viewport

---
+++ cypress/e2e/mobile.cy.ts
@@ -0,0 +1,26 @@
+// Realizando teste utilizando viewport para simular um dispositivo móvel (iPhone 6) e validar o processo de checkout no site saucedemo.com utilizando o framework Cypress
+// Objetivo é garantir que a aplicação em um viewport movel se comporte conforme esperado durante o processo de checkout, desde a adição de um item ao carrinho até a finalização da compra, incluindo a validação das mensagens de confirmação.
+describe('Mobile - Checkout', () => {
+
+  beforeEach(() => {
+    cy.viewport('iphone-6')
+
+    cy.visit('https://www.saucedemo.com')
+    cy.login('visual_user', 'secret_sauce')
+  })
+
+  it('should complete checkout on mobile', () => {
+    cy.get('[data-test^="add-to-cart"]').first().click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+
+    cy.get('[data-test="firstName"]').type('John')
+    cy.get('[data-test="lastName"]').type('Doe')
+    cy.get('[data-test="postalCode"]').type('12345')
+
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
+
+    cy.get('[data-test="complete-header"]').should('contain', 'Thank you for your order!')
+  })
+})
