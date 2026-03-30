25abd25b19c08357989ac431806811037409fe04
Aritse-stack <valter.mygame@gmail.com>
2026-03-30 16:08:18 -0300

    amend: add new feature, reports with cypress-mochawesome-reporter and cypress-multi-reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter',
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Cypress Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      reportDir: 'cypress/reports',
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
