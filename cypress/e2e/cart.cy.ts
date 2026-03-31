c1785b38af5c17feca7146a385eaff1c588f49bb
Aritse-stack <valter.mygame@gmail.com>
2026-03-31 14:33:03 -0300

    feat: add tests to multiple items in the cart and then remove each one.

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -4,7 +4,6 @@ describe('Cart', () => {
 
   // Login antes de cada teste
   // usando comando personalizado para login, onde se espera que os dados de usuario e login sejam validos.
-  // standard_user, performance_glitch_user, problem_user, error_user
   beforeEach(() => {
     cy.visit('https://www.saucedemo.com')
     cy.url().should('include', 'saucedemo.com')
@@ -40,5 +39,40 @@ describe('Cart', () => {
     cy.get('[data-test="inventory-item"]').should('have.length.greaterThan', 0)
     cy.get('[data-test^="remove"]').first().click()
     cy.get('[data-test="shopping-cart-badge"]').should('not.exist')
+  })
+
+  it('should attempt to add multiple items to the cart compare the names and them remove them', () => {
+    // Coletar os nomes dos produtos disponíveis
+    // Seletor alternativo [data-test^="item-"][data-test$="title-link"] || [data-test="inventory-item-name"]
+    cy.get('[data-test="inventory-item-name"]').then(($links) => {
+      const productsNames: string[] = []
+
+      $links.each((index, link) => {
+        productsNames.push(link.textContent?.trim() || '')
+      })
+
+      // Adicionar os produtos ao carrinho
+      cy.get('[data-test^="add-to-cart"]').each(($button) => {
+        cy.wrap($button).click()
+      })
+
+      // Verificar se os produtos adicionados correspondem aos produtos no carrinho
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.url().should('include', 'cart')
+
+      cy.get('[data-test="inventory-item-name"]').then(($cartItems) => {
+        const cartNames = [...$cartItems].map(el => el.textContent?.trim())
+
+        productsNames.forEach((name) => {
+          expect(cartNames).to.include(name)
+        })
+      })
+
+      // Remover os produtos do carrinho
+      cy.get('[data-test^="remove"]').each(($button) => {
+        cy.wrap($button).click()
+      })
+      cy.get('[data-test="shopping-cart-badge"]').should('not.exist')
+    })
   })
 })
