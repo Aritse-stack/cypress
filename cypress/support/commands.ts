6192f417240024feef2ba2595059f6ecbc94c7ab
Aritse-stack <valter.mygame@gmail.com>
2026-03-31 15:50:09 -0300

    fix: adicionado comentarios apropriado, atualizaçao dos comandos para serem mais determinísticos.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,19 +1,35 @@
 
-// Comando principal: realiza login com username e password fornecidos
+/*  
+  Comando principal: realiza login com username e password fornecidos
+
+  Comando auxiliar: realiza login utilizando um tipo de usuário definido na fixture users.json, facilitando a reutilização de dados de teste para diferentes cenários de login.
+
+  Os comandos parsonalizados foram designados para serem mais determinístico usando dados controlados, validação explicita de visibilidade dos campos e evitando conteudo dinamico ou dependente do tempo,
+    garantindo que os testes sejam mais confiáveis e menos propensos a falhas intermitentes. 
+*/
+
 Cypress.Commands.add('login', (username: string, password: string) => {
-  cy.get('[data-test="username"]').type(username)
-  cy.get('[data-test="password"]').type(password)
-  cy.get('[data-test="login-button"]').click()
+  cy.get('[data-test="username"]')
+    .should('be.visible')
+    .type(username)
+
+  cy.get('[data-test="password"]')
+    .should('be.visible')
+    .type(password)
+
+  cy.get('[data-test="login-button"]')
+    .should('be.visible')
+    .click()
 })
 
 // Login com qualquer tipo de usuário da fixture users.json
 // Exemplo: cy.loginAs('validUser') ou cy.loginAs('lockedOutUser')
 Cypress.Commands.add('loginAs', (userType: string) => {
-  cy.fixture('users').then((users) => {
-    const user = users[userType]
+  cy.fixture('users').its(userType).then((user) => {
     if (!user) {
       throw new Error(`User type "${userType}" not found in users.json`)
     }
+
     cy.login(user.username, user.password)
   })
 })
